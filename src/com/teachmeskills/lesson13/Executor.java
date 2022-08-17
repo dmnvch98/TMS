f7b5d98c6069a65ae8748616bb6e454e9ffd844b
dmn98 <dmnvch.evg@gmail.com>
2022-08-17 19:49:23 +0300

    Task 1 of lesson 13 almost done

---
+++ src/com/teachmeskills/lesson13/Executor.java
@@ -0,0 +1,7 @@
+package com.teachmeskills.lesson13;
+
+public class Executor {
+    public static void main(String[] args) {
+        DocumentNumberValidationReport.validateAllDocuments();
+    }
+}
